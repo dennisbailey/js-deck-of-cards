286200d8dfe346c7b278b8d472aac42a24bdd08b
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-15 14:51:11 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+var showCards = document.getElementById("cards");
+
+showCards.onclick = function(){
+  var cardContainer = document.getElementById('container');
+  cardContainer.innerHTML = "";
+  displayCards();
+};
+
+function displayCards(){
+  var deck = newDeck();
+  var shuffledCards = shuffleCards(deck);
+
+  for(var i=0; i < deck.length; i++){
+    var card = document.createElement('div');
+    card.className = "card";
+    var cardContainer = document.getElementById('container');
+    cardContainer.appendChild(card);
+    card.style.backgroundImage = "url(images/" + shuffledCards[i].suit + "-" + shuffledCards[i].card + ".png" + ")";
+
+  }
+}
+
+// Creates a deck of 52 cards
+function newDeck(){
+
+  var ranks = [
+    {card:"a"},
+    {card:"2"},
+    {card:"3"},
+    {card:"4"},
+    {card:"5"},
+    {card:"6"},
+    {card:"7"},
+    {card:"8"},
+    {card:"9"},
+    {card:"10"},
+    {card:"j"},
+    {card:"q"},
+    {card:"k"}
+  ];
+
+  var suits = [ "d", "c", "s", "h"];
+  var deck = [];
+}
+
+// Shuffles the Deck
+function shuffleCards(cardDeck){}
